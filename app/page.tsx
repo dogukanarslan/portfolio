483a614c6fd858755cd9e4663141a576e415ed80
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-03-09 00:08:45 +0300

    Add links

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,15 @@ export default function Home() {
         frontend development. Looking to develop responsive and user-friendly
         web applications along with clean code.
       </p>
+
+      <ul className="mt-8 flex gap-x-4 text-neutral-300">
+        <li>
+          <a href="https://github.com/dogukanarslan" className="hover:text-neutral-100">GitHub</a>
+        </li>
+        <li>
+          <a href="https://tr.linkedin.com/in/dogukanarslan">LinkedIn</a>
+        </li>
+      </ul>
     </div>
   );
 }
